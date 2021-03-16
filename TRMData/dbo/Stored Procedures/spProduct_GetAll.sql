0c43b7520315c720eedc566e96a9936873a5379c
Oumar Sall <o0sall01@louisville.edu>
2021-03-15 21:55:03 -0400

    Add Taxable To Products
    Adds the ability to tax (or not) individual products

--- TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
+++ TRMData/dbo/Stored Procedures/spProduct_GetAll.sql
@@ -3,7 +3,7 @@ AS
 BEGIN
 	set nocount on;
 
-	select Id, ProductName, [Description], RetailPrice, QuantityInStock
+	select Id, ProductName, [Description], RetailPrice, QuantityInStock, IsTaxable
 	from dbo.Product
 	order by ProductName;
 END
