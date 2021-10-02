4a1b531b974daef1bcdb001f26e24378abe8268e
Oumar Sall <o0sall01@louisville.edu>
2021-10-02 18:10:54 -0400

    Added Logging to the API

--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -13,9 +14,10 @@ namespace TRMDataManager.Library.Internal.DataAccess
 {
     public class SqlDataAccess : IDisposable, ISqlDataAccess
     {
-        public SqlDataAccess(IConfiguration config)
+        public SqlDataAccess(IConfiguration config, ILogger<SqlDataAccess> logger)
         {
             _config = config;
+            _logger = logger;
         }
         public string GetConnectionString(string name)
         {
@@ -71,6 +73,7 @@ namespace TRMDataManager.Library.Internal.DataAccess
 
         private bool isClose = false;
         private IConfiguration _config;
+        private readonly ILogger _logger;
 
         public void CommitTransaction()
         {
@@ -96,9 +99,9 @@ namespace TRMDataManager.Library.Internal.DataAccess
                 {
                     CommitTransaction();
                 }
-                catch
+                catch (Exception ex)
                 {
-                    // TODO - Log this issue
+                    _logger.LogError(ex, "Commit Transaction failed in the dispose method.");
                 }
             }
 
